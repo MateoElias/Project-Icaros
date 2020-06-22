4ac3d5756e01370829dcc66ba957d6b54bfe6d74
chaosinsurgent <61370647+chaosinsurgent@users.noreply.github.com>
2020-06-22 18:34:28 -0400

    Update announce.js

--- commands/Main/announce.js
+++ commands/Main/announce.js
@@ -8,8 +8,8 @@ module.exports = {
         let chnl = message.mentions.channels.first()
         if(!chnl) return message.channel.send("Please, specify the channel where you want the announcemen to be.")
         let MSG = message.content.split(" ").slice(2).join(" ")
-        if(MSG.lenght < 30) return message.channel.send("Text is too short to be announced.")
         if(!MSG) return message.channel.send("You did not specified anything to announce.")
+        if(MSG.lenght < 30) return message.channel.send("Text is too short to be announced.")
         const embed = new MessageEmbed()
             .setTitle(`**Announcement by **${message.member.displayName}:`)
             .setDescription(`${MSG}`)
@@ -17,7 +17,7 @@ module.exports = {
             .setFooter("Provided by Alexandra.AIC")
             .setTimestamp()
         
-            chnl.send("@here").then(m => m.delete({timeout: 2000}))
+        chnl.send("@here").then(m => m.delete({timeout: 3000}))
         chnl.send(embed)
     }
 }
