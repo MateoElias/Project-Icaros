e6c7ce294513c0179456a1f6d2a756e7525c16a2
chaosinsurgent <61370647+chaosinsurgent@users.noreply.github.com>
2020-06-25 14:56:59 -0400

    Loaded the test branch!

---
+++ commands/Moderation/clearwarns.js
@@ -0,0 +1,56 @@
+const warns = require('../../Models/warns');
+const Discord = require('discord.js');
+module.exports = {
+  name: "clearwarns",
+  description: "delete warns of a user",
+  category: "moderation",
+  usage: "<User mention> <Reason>",
+  run: async (client, message, args) => {
+    let user = message.mentions.users.first();
+    if (!user){
+        return message.channel.send(`⚠️ **Please state the user you want to clear warns of!** ⚠️`);
+    } 
+
+    warns.findOne(
+      { Guild: message.guild.id, User: user.id },
+      async (err, data) => {
+        if (err) console.log(err);
+        if (!data) {
+          let newWarns = new warns({
+            User: user.id,
+            Guild: message.guild.id,
+            Warns: [
+              {
+                Moderator: message.author.id,
+                Reason: args.slice(1).join(" "),
+              },
+            ],
+          });
+          newWarns.delete();
+          const embed2 = new Discord.MessageEmbed()
+            .setTitle('Warns cleared!')
+            .setDescription(`${user}'s warns have been cleared by ${message.author}!`)
+            .setColor('34cfeb')
+            .setFooter('Forgive and forget, that\'s what I always say!')
+            .setTimestamp()
+
+          message.channel.send(
+           embed2)
+        } else {
+          data.Warns.unshift({
+            Moderator: message.author.id,
+            Reason: args.slice(1).join(" "),
+          });
+          data.delete();
+          const embed = new Discord.MessageEmbed()
+            .setTitle('Warns cleared!')
+            .setDescription(`${user}'s warns have been cleared by ${message.author}!`)
+            .setColor('34cfeb')
+            .setFooter('Forgive and forget, that\'s what I always say!')
+            .setTimestamp()
+          message.channel.send(embed)
+        }
+      }
+    );
+  },
+};
---
+++ icaros.js
@@ -0,0 +1,44 @@
+const {Collection, Client, Discord} = require('discord.js');
+const fs = require('fs');
+const client = new Client;
+const mongoose = require('mongoose');
+const token = process.env.TOKEN;
+var prefix = "A!";
+client.commands = new Collection();
+client.aliases = new Collection();
+mongoose.connect("mongodb+srv://ProjectIcaros:BdgaP!68YaA9k5t@cluster0-wsyf8.mongodb.net/test",{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+client.categories = fs.readdirSync('./commands/');
+['command'].forEach(handler=>{
+    require(`./handler/${handler}`)(client);
+});
+client.on('ready',()=>{
+    console.log('Alexandra.AIC Is up and running!')
+    let botStatus = [
+        "A!help",
+        `over ${client.users.cache.size} users!`,
+        'the site\'s CCTV'
+      ]
+    
+    setInterval(function() {
+        let status = botStatus[Math.floor(Math.random() * botStatus.length)];
+        client.user.setActivity(status, {type: "WATCHING"});
+    
+        }, 7000)
+})
+client.on('message', async message =>{
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+    if(!message.guild) return;
+    if(!message.member) message.member = await message.guild.fetchMember(message);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLocaleLowerCase();
+    if(cmd.length == 0) return;
+    const command = client.commands.get(cmd)
+    if(!command) command = client.commands.get(client.aliases.get(cmd));
+    if(command) command.run(client,message,args)
+})
+
+client.login(token)
