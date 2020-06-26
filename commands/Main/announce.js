2435aa65de8b7ca805ad418d82e6d3d2128b0ef5
chaosinsurgent <61370647+chaosinsurgent@users.noreply.github.com>
2020-06-26 14:34:38 -0400

    Trying to fix.

--- commands/Main/announce.js
+++ commands/Main/announce.js
@@ -6,7 +6,7 @@ module.exports = {
     description: "Announcement Command",
     run: async (client, message, args) => {
         let chnl = message.mentions.channels.first()
-        if (!chnl) return message.channel.send("Please, specify the channel where you want the announcement to be.")
+        if (!chnl) return message.channel.send("Please, specify the channel where you want the announcemen to be.")
         let MSG = message.content.split(" ").slice(2).join(" ")
         if (!MSG) return message.channel.send("You did not specified anything to announce.")
         const embed = new MessageEmbed()
@@ -16,11 +16,7 @@ module.exports = {
             .setFooter("Provided by Alexandra.AIC")
             .setTimestamp()
 
-            const timeout = 1000
-            setTimeout(() => {
-                chnl.send('@here'),
-                message.delete('@here'),
-                chnl.send(embed)
-            }, timeout);
+            chnl.send("@here").then(m => m.delete());
+            chnl.send(embed)
     }
 }
