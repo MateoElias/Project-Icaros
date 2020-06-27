a8df342945821f288159e3e32e8157178d145e50
Mateo Elias <hadadmateo1@gmail.com>
2020-06-26 23:59:14 -0400

    Fixed.
    I was careless...

--- commands/Main/ping.js
+++ commands/Main/ping.js
@@ -3,12 +3,13 @@ module.exports = {
     name: 'ping',
     description: 'shows ping!',
     run: async (bot, message, args) => {
-        const msg = await message.channel.send("@here").then(m => m.delete());
+        const msg = await message.channel.send('Pinging...')
         const embed = new Discord.MessageEmbed()
-        .setTitle('Pong!')
+        .setTitle(':ping_pong:Pong!:ping_pong:')
         .setDescription(`Ping is ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}MS\nAPI ping is ${Math.round(bot.ws.ping)}MS`)
         .setColor('34cfeb')
         msg.edit(embed)
-        message.delete()
     }
+    
+
 }
