b730cd962d9381aeedcb716ab2fbc99a9fc733c7
chaosinsurgent <61370647+chaosinsurgent@users.noreply.github.com>
2020-06-28 22:03:37 -0400

    help

--- icaros.js
+++ icaros.js
@@ -11,15 +11,38 @@ mongoose.connect(process.env.mongodb, {
     useNewUrlParser: true,
 });
 client.categories = fs.readdirSync('./commands/');
-['command', 'event'].forEach(handler => {
+['command'].forEach(handler => {
     require(`./handler/${handler}`)(client);
 });
-client.on("ready", () => {
-  require("./events/client/ready")(client);
-});
-client.on("message", async (message) => {
-  message.member; //-- GuildMember based
-  message.author; //-- User based
-  require("./events/guild/message")(client, message);
-});
+    client.on('ready', () => {
+        console.log('Alexandra.AIC Is up and running!')
+        let botStatus = [
+            `${prefix}help`,
+            `over ${client.users.cache.size} users!`,
+            'the site\'s CCTV'
+        ]
+    
+        setInterval(function () {
+            let status = botStatus[Math.floor(Math.random() * botStatus.length)];
+            client.user.setActivity(status, {
+                type: "WATCHING"
+            });
+    
+        }, 7000)
+    }) 
+}
+client.on('message', async message => {
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+        if (!message.guild) return;
+        if (!message.member) message.member = await message.guild.fetchMember(message);
+        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+        const cmd = args.shift().toLocaleLowerCase();
+        if (cmd.length == 0) return;
+        const command = client.commands.get(cmd)
+        if (!command) command = client.commands.get(client.aliases.get(cmd));
+        if (command) command.run(client, message, args)
+    })
+
+}
 client.login(token)
