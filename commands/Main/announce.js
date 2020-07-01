d0945c9af8e4da3376b1e48d66fdf5d8db378505
chaosinsurgent <61370647+chaosinsurgent@users.noreply.github.com>
2020-06-30 23:27:17 -0400

    Update announce.js

--- commands/Main/announce.js
+++ commands/Main/announce.js
@@ -5,10 +5,20 @@ module.exports = {
     name: "announce",
     description: "Announcement Command",
     run: async (client, message, args) => {
+
+        // Channel Definition
         let chnl = message.mentions.channels.first()
         if (!chnl) return message.channel.send("Please, specify the channel where you want the announcemen to be.")
+        
+        //Message Definition
         let MSG = message.content.split(" ").slice(2).join(" ")
+        var short = new MessageEmbed()
+        .setTitle('Your announcement is too short to be announced.')
+        .setFooter('Make sure your your announcement is longer than 30 characters!')
+        .setColor('34cfeb')
+        if (MSG.length < 30) return message.channel.send(short)
         if (!MSG) return message.channel.send("You did not specified anything to announce.")
+
         const embed = new MessageEmbed()
             .setTitle(`**Announcement by **${message.member.displayName}:`)
             .setDescription(`${MSG}`)
