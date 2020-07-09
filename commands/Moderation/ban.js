a9367a765cfaef7e5bdf004e18ce85d794cc221d
Mateo Elias <hadadmateo1@gmail.com>
2020-07-09 13:25:35 -0400

    Update ban.js

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -4,62 +4,20 @@ module.exports = {
     description: "Bans a designated user out of the current guild.",
     run: async (client, message, args) => {
 
-        if (!message.member.hasPermission('BAN_MEMBERS')) return;
-	    //e
-
-        let user = message.mentions.users.first()
-        const member = message.guild.member(user);
-
-        var permerror = new Discord.MessageEmbed()
-            .setTitle("I cannot ban that user!")
-            .setFooter("I cannot ban this user, most likely because that user is also a mod, check the user's permissions and try again.")
-            .setColor('34cfeb')
-
-        if(user.hasPermission('BAN_MEMBERS')) return (message.channel.send(permerror)).then(message.delete())
-
-        var error1 = new Discord.MessageEmbed()
-            .setTitle("You did not mentioned the user you wanted to bam!")
-            .setFooter("Remember to mention the user right before the command! \"A!ban @user\" ")
-            .setColor('34cfeb')
-
-        if (!user) return (message.channel.send(error1)).then(message.delete())
-
-        let reason = args.slice(1).join(" ")
-        var error2 = new Discord.MessageEmbed()
-            .setTitle("You did not specified a reason!")
-            .setFooter("Remember to specify the reason after the user has been mentioned! \"@user [REASON]\"")
-            .setColor('34cfeb')
-
-        if (!reason) return (message.channel.send(error2)).then(message.delete())
-
-        var success = new Discord.MessageEmbed()
-            .setTitle("Success!")
-            .setDescription(`**${user.tag}** has been banned successfully. \n You can see more of the details below:`)
-            .addFields({
-                name: "__Moderator:__",
-                value: `\`${message.member.displayName}\``,
-                inline: true
-            }, {
-                name: "__Reason:__",
-                value: `${reason}`,
-                inline: true
-            })
-            .setFooter("Did we really had to get to that point? Of course not! That was HIS choice.")
-            .setColor('34cfeb')
-
-        var error3 = new Discord.MessageEmbed()
-            .setTitle("Error")
-            .setDescription("I was unable to find that user, maybe it's not on the server anymore.")
-            .setFooter("Remember to properly mention the user, or check if the user is on the server.")
-            .setColor('34cfeb')
+        if (!message.member.hasPermission('BAN_MEMBERS')) {
+            return message.channel.send(`You are unable to ban members`)
+        }
+        if (!args[0]) {
+            return message.channel.send(`Please mention a user!`)
+        }
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
         try {
-            await member.ban(reason);
-            await message.channel.send(success)
+            await member.ban();
+            await message.channel.send(`${member} has been banned!`)
         } catch (e) {
-            return message.channel.send(error3)
+            return message.channel.send(`User is not in the server!`)
         }
-	message.delete()
-    }
 
+    }
 }
