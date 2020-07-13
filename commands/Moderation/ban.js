d1765e996340f4ad5837505166a6d0e410f9f5ed
chaosinsurgent <61370647+chaosinsurgent@users.noreply.github.com>
2020-07-13 04:06:45 -0400

    Upgrades.

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -4,20 +4,62 @@ module.exports = {
     description: "Bans a designated user out of the current guild.",
     run: async (client, message, args) => {
 
-        if (!message.member.hasPermission('BAN_MEMBERS')) {
-            return message.channel.send(`You are unable to ban members`)
-        }
-        if (!args[0]) {
-            return message.channel.send(`Please mention a user!`)
-        }
-        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        if (!message.member.hasPermission('BAN_MEMBERS')) return;
 
+        var user = message.mentions.members.first()
+        const member = message.guild.member(user)
+
+        if(member === message.author) return;
+
+        var error1 = new Discord.MessageEmbed()
+            .setTitle("You did not mention the user you wanted to ban!")
+            .setFooter("Remember to mention the user right before the command! \"A!ban @user\" ")
+            .setColor('34cfeb')
+
+        if (!member) return (message.channel.send(error1)).then(message.delete())
+
+        var admintoo = new Discord.MessageEmbed()
+        .setTitle("This user is a moderator too!")
+        .setFooter("The user's permissions prevent me from banning the specified user.")
+        .setColor('34cfeb')
+        if(member.hasPermission('BAN_MEMBERS')) return message.channel.send(admintoo)
+
+        let reason = args.slice(1).join(" ");
+        var error2 = new Discord.MessageEmbed()
+            .setTitle("You did not specified a reason!")
+            .setFooter("Remember to specify the reason after the user has been mentioned! \"@user [REASON]\"")
+            .setColor('34cfeb')
+
+        if (!reason) return (message.channel.send(error2)).then(message.delete())
+
+        var success = new Discord.MessageEmbed()
+        .setTitle("Success!")
+            .setDescription(`**${user.tag}** has been banned successfully. \n You can see more of the details below:`)
+            .addFields({
+                name: "__Moderator:__",
+                value: `\`${message.member.displayName}\``,
+                inline: true
+            }, {
+                name: "__Reason:__",
+                value: `${reason}`,
+                inline: true
+            })
+            .setFooter("Welp! Try not to be like that guy!")
+            .setColor('34cfeb')
+
+            var error3 = new Discord.MessageEmbed()
+            .setTitle("Error")
+            .setDescription("I was unable to find that user, maybe it's not on the server anymore.")
+            .setFooter("Remember to properly mention the user, or check if the user is on the server.")
+            .setColor('34cfeb')
+        
         try {
-            await member.ban();
-            await message.channel.send(`${member} has been banned!`)
+            await user.ban();
+            await message.channel.send(success);
         } catch (e) {
-            return message.channel.send(`User is not in the server!`)
+            return message.channel.send(error3)
         }
 
     }
+
 }
--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -6,11 +6,20 @@ module.exports = {
 
         if (!message.member.hasPermission('KICK_MEMBERS')) return;
 
-
         let user = message.mentions.users.first()
         const member = message.guild.member(user);
+
+        if (member === message.author) return; 
+
+        var permerror = new Discord.MessageEmbed()
+            .setTitle("I cannot kick that user!")
+            .setFooter("I cannot kick this user, most likely because that user is also a mod, check the user's permissions and try again.")
+            .setColor('34cfeb')
+
+        if(user.hasPermission('KICK_MEMBERS')) return (message.channel.send(permerror)).then(message.delete())
+
         var error1 = new Discord.MessageEmbed()
-            .setTitle("You did not mentioned the user you wanted to kick!")
+            .setTitle("You did not mention the user you wanted to kick!")
             .setFooter("Remember to mention the user right before the command! \"A!kick @user\" ")
             .setColor('34cfeb')
 
@@ -45,23 +54,8 @@ module.exports = {
             .setFooter("Remember to properly mention the user, or check if the user is on the server.")
             .setColor('34cfeb')
 
-        var dm = new Discord.MessageEmbed()
-            .setAuthor(`You have been kicked from: ${message.guild.name}`, message.guild.iconURL())
-            .setDescription(`You have recieved a warning in ${message.guild.name}! \n You can see more of the details below`)
-            .addFields({
-                name: '__Moderator:__',
-                value: `\` ${message.member.displayName} \``
-            }, {
-                name: '__Reason:__',
-                value: `**${reason}**`
-            })
-            .setColor('34cfeb')
-            .setFooter('Well, that was one wayt to go, wasn\'t it?')
-            .setTimestamp()
-        member.send(dm)
-
         try {
-            await member.kick(reason).then(member.send(dm));
+            await member.kick(reason);
             await message.channel.send(success)
         } catch (e) {
             return message.channel.send(error3)
