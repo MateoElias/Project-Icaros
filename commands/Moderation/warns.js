77a664252c2e366296e8a48c69e07b0c2f6d140d
Mateo Elias <hadadmateo1@gmail.com>
2020-07-17 06:37:07 -0400

    Update warns.js

--- commands/Moderation/warns.js
+++ commands/Moderation/warns.js
@@ -1,32 +1,25 @@
-const warns = require("../../Models/warns");
+const warns = require("../../models/warns");
 const { MessageEmbed } = require("discord.js");
 module.exports = {
   name: "warns",
   description: "Get a user's warns in the guild!",
   category: "moderation",
   usage: "<User mention>",
-  run: async (client, message, args) => {
-    let meap = message.mentions.members.first();
-    if (!meap) return message.channel.send(`No user specified!`);
+  run: async (bot, message, args) => {
+    let user = message.mentions.members.first();
+    if (!user) return message.channel.send(`No user specified!`);
     warns.find(
-      { Guild: message.guild.id, User: meap.id },
+      { Guild: message.guild.id, User: user.id },
       async (err, data) => {
         if (err) console.log(err);
         if (!data.length)
           return message.channel.send(
-            `${meap.tag} has not got any warns in this guild!`
+            `${user.user.tag} has not got any warns in this guild!`
           );
         let Embed = new MessageEmbed()
-          .setTitle(`${meap.tag}'s warns in ${message.guild.name}.. `)
-          .setDescription(
-            data.map((d) => {
-              return d.Warns.map(
-                (w, i) =>
-                  `${i} - Moderator: ${
-                    message.guild.members.cache.get(w.Moderator).user.tag
-                  } Reason: ${w.Reason}`
-              ).join("\n");
-            })
+          .setTitle(`${user.user.tag}'s warns in ${message.guild.name}.. `)
+          .setDescription(data.map(d=>{
+                return d.Warns.map(w=>`| Moderator: ${message.guild.members.cache.get(w.Moderator).user.tag} | Reason: ${w.Reason}`).join("\n")}))
           );
         message.channel.send(Embed);
       }
