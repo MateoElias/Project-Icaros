60bf89f01473a80a56ce71429d266d8b04d92c5c
Mateo Elias <hadadmateo1@gmail.com>
2020-07-21 14:08:42 -0400

    Update kick.js

--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -32,7 +32,7 @@ module.exports = {
 
         var success = new Discord.MessageEmbed()
             .setTitle("Success!")
-            .setDescription(`You can see more of the details below: \n**${user.tag}** has been banned successfully.`)
+            .setDescription(`**${user}** has been banned successfully. \n You can see more of the details below:`)
             .addFields({
                 name: "__Moderator:__",
                 value: `\`${message.member.displayName}\``,
