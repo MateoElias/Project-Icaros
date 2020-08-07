b8f227ac5e4cbd1d05efc3725eb426957ea4ebf9
chaosinsurgent <61370647+chaosinsurgent@users.noreply.github.com>
2020-08-07 17:09:10 -0400

    Update ping.js

--- commands/Main/ping.js
+++ commands/Main/ping.js
@@ -3,11 +3,16 @@ module.exports = {
     name: 'ping',
     description: 'shows ping!',
     run: async (client, message, args) => {
-        const embed = new Discord.MessageEmbed()
-        .setTitle('Pong!')
-        .setDescription(`Ping is ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}MS\nAPI ping is ${Math.round(bot.ws.ping)}MS`)
-        .setColor('34cfeb')
-        message.channel.send("Pinging . . .").then(m => m.delete()).then(message.channel.send(embed))
-        message.delete()
-    }
-}
+        message.channel.send(`Pinging....`).then((msg) => {
+      const _ = new Discord.MessageEmbed()
+        .setTitle("Pong!")
+        .setDescription(
+          `Pong!\nLatency is ${Math.floor(
+            msg.createdTimestamp - message.createdTimestamp
+          )}ms\nAPI Latency is ${Math.round(bot.ws.ping)}ms`
+        )
+        .setColor("34cfeb");
+      msg.edit(_);
+    });
+  },
+};
