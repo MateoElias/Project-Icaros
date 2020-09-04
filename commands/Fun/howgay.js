69e1c93ec982f318acc184d8450a5e32249df9e4
Mateo Elias <hadadmateo1@gmail.com>
2020-09-04 16:09:08 -0400

    Update howgay.js

--- commands/Fun/howgay.js
+++ commands/Fun/howgay.js
@@ -3,32 +3,23 @@ module.exports = {
   name: "howgay",
   descriptions: "Shows how gay you are lmao",
   run: async(client, message, args) => {
-
-    async function arrayToString(arr) {
-      var str = " ";
-      for (let i = 0; i < arr.length - 1; i++)
-        str += arr[i] + " ";
-      str += arr[arr.length-1];
-      return str;
-    }
-
-      var name = message.mentions.users.first || message.member.displayName()
-
-if (args[0] && !message.mentions.users.first)
-  name = arrayToString(args);
+    
+    var name = message.mentions.users.first()
     
       var percent = Math.floor(Math.random() * 100);
-    
       var resp = new MessageEmbed()
         .setTitle("Homosexuality Rate")
         .setColor('RANDOM')
         .setDescription(`🏳️‍🌈 ${name} is ${percent}% gay 🏳️‍🌈`);
     
-      if (message.channel)
-        message.channel.send(resp);
-      else
-        message.reply(resp);
-    
-      return;
+    if(!name) {
+    resp = new MessageEmbed()
+        .setTitle("Homosexuality Rate")
+        .setColor('RANDOM')
+        .setDescription(`🏳️‍🌈 ${message.author.username} is ${percent}% gay 🏳️‍🌈`);
+     message.channel.send(resp)
+    } else { 
+      message.channel.send(resp)
+    }
   }
 }
