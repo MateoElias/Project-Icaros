47c3ac49d85869555395f75ee43d44efd23c0643
chaosinsurgent <61370647+chaosinsurgent@users.noreply.github.com>
2020-09-04 14:59:27 -0400

    Howgay command lmao

---
+++ commands/Fun/howgay.js
@@ -0,0 +1,34 @@
+const { MessageEmbed } = require('discord.js')
+module.exports = {
+  name: "howgay",
+  descriptions: "Shows how gay you are lmao",
+  run: async(client, message, args) => {
+
+    async function arrayToString(arr) {
+      var str = " ";
+      for (let i = 0; i < arr.length - 1; i++)
+        str += arr[i] + " ";
+      str += arr[arr.length-1];
+      return str;
+    }
+
+      name = message.author.username;
+    
+      if (args[0])
+        name = arrayToString(args);
+    
+      var percent = Math.floor(Math.random() * 100);
+    
+      var resp = new MessageEmbed()
+        .setTitle("Homosexuality Rate")
+        .setColor('RANDOM')
+        .setDescription(`🏳️‍🌈 ${name} is ${percent}% gay 🏳️‍🌈`);
+    
+      if (message.channel)
+        message.channel.send(resp);
+      else
+        message.reply(resp);
+    
+      return;
+  }
+}
