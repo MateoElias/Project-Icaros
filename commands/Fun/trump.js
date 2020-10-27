c7f2bcefdfefaddcd8dc971e141716d4d06710fa
Mateo Elias <hadadmateo1@gmail.com>
2020-10-26 21:17:00 -0400

    Update trump.js

--- commands/Fun/trump.js
+++ commands/Fun/trump.js
@@ -6,8 +6,9 @@ module.exports = {
     run: async (client, message, args) => {
 
         const MSG = message.content.split(" ").slice(1).join(" ");
+        if(!MSG) return message.channel.send("Please input any message")
 
-        const data = await fetch(`https://api.no-api-key.com/api/v2/trump/?message=${MSG}`)
+        const data = await fetch(`https://api.no-api-key.com/api/v2/trump/?message=${MSG}`).catch(err => message.channel.send(err))
         
         const embed = new MessageEmbed()
         .setColor('34cfeb')
