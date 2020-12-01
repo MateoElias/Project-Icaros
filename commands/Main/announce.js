ae8c5ce546f127ca3ff0f2fa09df8a985107612b
Mateo Elias <hadadmateo1@gmail.com>
2020-12-01 10:23:59 -0400

    Update announce.js

--- commands/Main/announce.js
+++ commands/Main/announce.js
@@ -1,16 +1,14 @@
-const {
-    MessageEmbed,
-} = require('discord.js');
+const Discord = require('discord.js');
 module.exports = {
     name: "announce",
     description: "New announcement command",
     run: async (client, message, args) => {
 
-        const sending1 = new MessageEmbed()
+        const sending1 = new Discord.MessageEmbed()
         .setTitle("Sending . . .")
         .setColor('ffbb17')
 
-    var sent = new MessageEmbed()
+    var sent = new Discord.MessageEmbed()
         .setTitle("Success!")
         .setDescription('Your announcement has been published successfully.')
         .setColor('4cb913')
@@ -23,14 +21,14 @@ module.exports = {
 
         //VARIABLE DEFINITION
         var MSG = message.content.split(" ").slice(3).join(" ")
-        var short = new MessageEmbed()
+        var short = new Discord.MessageEmbed()
         .setTitle('Your announcement is too short to be announced.')
         .setFooter('Make sure your your announcement is longer than 10 characters!')
         .setColor('c70808')
         if (MSG.length < 10) return message.channel.send(short).then(message.delete())
         if (!MSG) return message.channel.send("You did not specified anything to announce.").then(message.delete())
         const chnl = message.mentions.channels.first();
-        var nochnl = new MessageEmbed()
+        var nochnl = new Discord.MessageEmbed()
         .setTitle('You did not specified the channel you want the announcement in.')
         .setFooter('Pleas specify the channel right after the command. "A!announce #channel"')
         .setColor('c70808')
@@ -40,7 +38,7 @@ module.exports = {
         
 
         //ANNOUNCEMENT
-        const embed = new MessageEmbed()
+        const embed = new Discord.MessageEmbed()
             .setTitle(`**Announcement by **${message.member.displayName}:`)
             .setAuthor(".|SCPF|. Special Containment Procedures Foundation", message.guild.iconURL(), 'https://www.roblox.com/groups/5137119/Special-Containment-Procedures-Foundation-SCPF#!/about')
             .setDescription(`${MSG}`)
@@ -48,14 +46,14 @@ module.exports = {
             .setTimestamp()
             .setFooter("Alexandra.AIC | Developed by: O5-6", 'https://cdn.discordapp.com/attachments/667913030629195786/728325820715892736/Alexandra.png')
 
-        const areusure = new MessageEmbed()
+        const areusure = new Discord.MessageEmbed()
             .setTitle("Are you sure you want to send this announcement?")
             .setDescription(`${MSG} \n __Using the **${ping.toUpperCase()}** ping in ${chnl}?__`)
             .setColor('ffbb17')
             .setThumbnail('https://cdn.discordapp.com/attachments/730670936088641559/761689831561625630/alex_sad.png')
             .setFooter('Use the "❌" and "✅" to confirm or deny.')
 
-        const noping = new MessageEmbed()
+        const noping = new Discord.MessageEmbed()
             .setTitle("You did not specify the ping to utilize!")
             .setFooter("A!announce #channel `<here/all/null>`")
             .setColor('c70808')
