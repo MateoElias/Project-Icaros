b27080b0c78440c6d6a5087c7921014962f01c9e
Mateo Elias <hadadmateo1@gmail.com>
2021-01-14 18:55:21 -0400

    Update createCard.js

--- trello/createCard.js
+++ trello/createCard.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch')
 function createCard(message, key, token, id) {
 
-    fetch(`https://api.trello.com/1/lists?key=${key}&token=${token}&name=${message}&idBoard=${id}`, {
-  method: 'POST'
+    fetch(`https://api.trello.com/1/cards?key=${key}&token=${token}&idList=6000bb6055b65014068b3ad2&name=${message}`, {
+    method: 'POST'
     }).then(res => {
         res = "Successfully created card"
         return console.log(res)
