6bc6ca4a6ebe1ba1e7cf4d09c89a84e1f52e14c4
Mateo Elias <hadadmateo1@gmail.com>
2021-01-18 10:38:01 -0400

    Update icaros.js

--- icaros.js
+++ icaros.js
@@ -43,11 +43,11 @@ client.categories = fs.readdirSync('./commands/');
     
 client.on('message', async message => {
         if (message.author.bot) return;
-        if (!message.content.startsWith(prefix.toLowerCase())) return;
+        if (!message.content.startsWith(prefix)) return;
         if (!message.guild) return;
         if (!message.member) message.member = await message.guild.fetchMember(message);
         const args = message.content.slice(prefix.length).trim().split(/ +/g);
-        const cmd = args.shift().toLocaleLowerCase().toLowerCase();
+        const cmd = args.shift().toLowerCase();
         if (cmd.length == 0) return;
         const command = client.commands.get(cmd)
         if (!command) command = client.commands.get(client.aliases.get(cmd));
