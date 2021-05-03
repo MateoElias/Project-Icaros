ca00e7aa3a291e0af503b04f205691c9d413781b
Mateo Elias <hadadmateo1@gmail.com>
2021-05-03 12:54:19 -0400

    God.. what would I do without you, version control

--- commands/Verification/newverify.js
+++ commands/Verification/newverify.js
@@ -1,7 +1,6 @@
 const http = require('node-fetch')
 const config = require('../../config.json')
-const nbx = require('noblox.js')
-const ms = require('ms')
+const noblox = require('noblox.js')
 const Discord = require('discord.js')
 
 module.exports = {
@@ -10,36 +9,36 @@ module.exports = {
     run: async(client, message, args) => {
 
         //Get name Data
-        var apiData = await http(`https://verify.nezto.re/api/roblox/${message.author.id}`); apiData = await apiData.json()
+        var apiData = await http(`https://verify.eryn.io/api/user/${message.author.id}`); apiData = await apiData.json()
         var botMessage = new Discord.MessageEmbed()
 
 
         async function mainGroupHandler(robloxId, groupId){
 
-            const isInGroup = await nbx.getRankInGroup(groupId, robloxId) != 0
+            const isInGroup = await noblox.getRankInGroup(groupId, robloxId) != 0
 
             if(isInGroup){
-                const rankName = await nbx.getRankNameInGroup(groupId, robloxId)
+                const rankName = await noblox.getRankNameInGroup(groupId, robloxId)
                 const guild = message.guild
 
                 var role = guild.roles.cache.find(r => r.name == rankName);
 
+                message.member.roles.remove(message.member.roles.cache.some(r => r.name == rankName))
                 try {
-                    await GuildMember.roles.remove(GuildMember.roles.cache)
-                    await GuildMember.roles.add(role)
+                    await message.member.roles.add(role)
                 } catch (err) {
                     message.channel.send("I am unable to find a role to give.")
                 }
             } else {
-                GuildMember.roles.remove(GuildMember.roles.cache)
-                GuildMember.roles.add(message.guild.roles.cache.find(r => r.name == 'Class D'))
+                message.member.roles.remove(message.member.roles.cache)
+                message.member.roles.add(r => r.name == 'Class D')
             }
         }
 
-        if (apiData.sucess == true) {
+        if (apiData.status == 'ok') {
 
             botMessage.setTitle("Verification Successful")
-            botMessage.setDescription(`You are currently verified as **${apiData.robloxUsername}**. If you wish to reverify your account, click [here](https://verify.nezto.re/discord) and follow the instructions.`)
+            botMessage.setDescription(`You are currently verified as **${apiData.robloxUsername}**. If you wish to reverify your account, click [here](https://verify.eryn.io/) and follow the instructions.`)
             botMessage.setColor('4cb913')
     
             try {
@@ -53,21 +52,21 @@ module.exports = {
             mainGroupHandler(apiData.robloxId, config.robloxGroups.main)
 
         } else {
-            switch (apiData.error.status) {
+            switch (apiData.errorCode) {
                 case 404:
                     botMessage.setTitle("User Not Found")
                     botMessage.setColor("c70808")
-                    botMessage.setDescription(`Your discord account isn't linked to a Roblox account. To verify your account, click [here](https://verify.nezto.re/discord) and follow the instructions.`)
+                    botMessage.setDescription(`Your discord account isn't linked to a Roblox account. To verify your account, click [here](https://verify.eryn.io/) and follow the instructions.`)
                     break;
 
                 case 429:
                     botMessage.setTitle("Too Many Requests")
                     botMessage.setColor("c70808")
-                    botMessage.setDescription(`The API is currently receiving too many requests from various clients, please retry after **${ms(apiData.error.retryAfter, { long: true })}** seconds`)
+                    botMessage.setDescription(`The API is currently receiving too many requests from various clients, please retry after **${apiData.retryAfterSeconds}** seconds`)
                     break;
 
                 default:
-                    botMessage.setTitle(`${apiData.error.status} Error`)
+                    botMessage.setTitle(`${apiData.errorCode} Error`)
                     botMessage.setDescription("An error has occured, please try again after a few minutes, if the error persists, contact server administration.")
                     botMessage.setColor("c70808")
             }
