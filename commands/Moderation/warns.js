b6dc3b22b5ca3ae9c248e228edc61798a7fdd006
Mateo Elias <hadadmateo1@gmail.com>
2021-05-03 20:43:16 -0400

    Update warns.js

--- commands/Moderation/warns.js
+++ commands/Moderation/warns.js
@@ -29,7 +29,7 @@ module.exports = {
               return d.Warns.map(
                 (w, i) =>
                   `${i+1} - **Moderator**: ${
-                    message.guild.members.cache.find(n => n.user.username == w.Moderator).nickname
+                    message.guild.members.cache.find(n => n.user.username == w.Moderator)
                   } **Reason:** __${w.Reason}__`
               ).join("\n");
             })
