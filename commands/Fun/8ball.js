0f7f4ebf20b610000d84d4c7391e9f79e6cfa773
Mateo Elias <hadadmateo1@gmail.com>
2021-05-05 20:27:26 -0400

    Update 8ball.js

--- commands/Fun/8ball.js
+++ commands/Fun/8ball.js
@@ -5,11 +5,17 @@ module.exports = {
   name: "8ball",
   descriptions: "magic vodoo stuff idk man",
   run: async(client, message, args) => {
-    const msg = message.content.split(" ").slice(0).join(" ")
+    const msg = message.content.split(" ").slice(1).join(" ")
     
     if(!msg) return message.reply("This time actually ask me something")
     
     console.log(msg)
+    
+    fetch(`https://8ball.delegator.com/magic/JSON/${msg}`)
+    .then(res => res.json())
+    .then(answer => {
+      console.log(answer)
+    })
     
   }
 }
